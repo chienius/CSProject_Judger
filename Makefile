4329376f30b76d07530f547da17a8b648817f469
Archie Lu <chienius@outlook.com>
2014-11-24 21:09:30 +0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+all: 
+	g++ judger.cpp -pthread -lmongoclient -lboost_thread -lboost_system -lboost_regex -o judger -lboost_filesystem -lboost_program_options
+
+clean:
+	rm judger -f
---
+++ judger.cpp
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  judger.cpp
+ *
+ *    Description:  Kyuyu Judger
+ *
+ *        Version:  1.0
+ *        Created:  2014年11月24日 21时08分12秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Archie Lu (), chienius@outlook.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
